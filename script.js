68970c997aa10deebf363fb84640f3a5856ccbcd
Kierby M. Antonio <kantonio_250000000109@uic.edu.ph>
2026-09-15 13:42:24 +0800

    Fix regex patterns for image and text formatting

--- script.js
+++ script.js
@@ -20,15 +20,14 @@ function convertMarkdown() {
 
   let html = lines.join('');
 
-  html = html.replace(/!\[([^\]])\]\(([^)])\)/g, '<img alt="$1" src="$2">');
+  html = html.replace(/!\[([^\]]*)\]\(([^)]*)\)/g, '<img alt="$1" src="$2">');
 
   html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
 
-  html = html.replace(/\\(.?)\\*/g, '<strong>$1</strong>');
+  html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
   html = html.replace(/__(.*?)__/g, '<strong>$1</strong>');
 
-  html = html.replace(/\(.?)\*/g, '<em>$1</em>');
-  html = html.replace(/(.*?)/g, '<em>$1</em>');
+  html = html.replace(/\*(.*?)\*/g, '<em>$1</em>');
 
   const htmlOutput = document.getElementById('html-output');
   const preview = document.getElementById('preview');
